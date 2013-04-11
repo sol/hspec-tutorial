5a64b79be54f771a0231a8eb34c22fcf8e19b31d
Simon Hengel <sol@typeful.net>
2013-04-12 01:44:17 +0200

    Solution for problem 3

--- src/App.hs
+++ src/App.hs
@@ -15,7 +15,17 @@ data Service = Service {
 
 instance ToJSON Service
 
-app :: IO Application
-app = scottyApp $ do
+data Response = Response {
+  current_time :: String
+} deriving (Eq, Show, Generic)
+
+instance ToJSON Response
+
+app :: IO UTCTime -> IO Application
+app getTime = scottyApp $ do
   get "/" $ do
     json (Service "time-service" "0.1.0")
+
+  get "/current-time.json" $ do
+    t <- liftIO getTime
+    json (Response $ show t)
--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -12,13 +12,17 @@ import           Data.String
 import           App (app)
 
 spec :: Spec
-spec = with app $ do
+spec = with (app $ readIO "2013-04-11 21:37:58 UTC") $ do
   describe "GET /" $ do
     it "responds with HTTP status 200" $ do
       get "/" `shouldRespondWith` 200
 
     it "returns service name and version" $ do
       get "/" `shouldRespondWith` [json|{name: "time-service", version: "0.1.0"}|]
+
+  describe "GET /current-time.json" $ do
+    it "returns the current time" $ do
+      get "/current-time.json" `shouldRespondWith` [json|{current_time: "2013-04-11 21:37:58 UTC"}|]
 
   context "when given an invalid request path" $ do
     it "responds with HTTP status 404" $ do
