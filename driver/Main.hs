0211cbd34c57f425ffff214c1d0fd2f7625b321e
Simon Hengel <sol@typeful.net>
2013-04-12 01:44:17 +0200

    Solution for problem 3

--- driver/Main.hs
+++ driver/Main.hs
@@ -2,8 +2,9 @@ module Main (main) where
 
 import           App
 import           Network.Wai.Handler.Warp
+import           Data.Time
 
 main :: IO ()
 main = do
   putStrLn "http://localhost:3000"
-  app >>= run 3000
+  app getCurrentTime >>= run 3000
