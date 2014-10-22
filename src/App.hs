287cf6cdc971d8ac8a328ea3a3c39672f4ad186d
Simon Hengel <sol@typeful.net>
2014-10-22 13:39:55 +0800

    Update for wai 3

--- src/App.hs
+++ src/App.hs
@@ -3,9 +3,7 @@ module App where
 
 import           Data.Data
 import           Data.JSON (toJSON)
-import           Data.Conduit
-import           Blaze.ByteString.Builder.ByteString (fromByteString)
-import           Data.Conduit.List (sourceList)
+import qualified Data.ByteString.Lazy as LB
 import           Web.Scotty hiding (json)
 import           Network.Wai
 
@@ -16,7 +14,7 @@ data Message = Message {
 json :: Data a => a -> ActionM ()
 json data_ = do
   addHeader "Content-Type" "application/json"
-  (source . sourceList . return . Chunk . fromByteString . toJSON) data_
+  raw (LB.fromStrict $ toJSON data_)
 
 app :: IO Application
 app = scottyApp $ do
