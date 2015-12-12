62d2ea3a35c194cce15f23a3c95ca6de8cfdd46a
Simon Hengel <sol@typeful.net>
2015-12-12 10:45:41 +0800

    Solution for problem 4

--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -11,6 +11,12 @@ import           Data.String
 
 import           App (app)
 
+data InvalidPath = InvalidPath ByteString
+  deriving Show
+
+instance Arbitrary InvalidPath where
+  arbitrary = InvalidPath . fromString <$> (listOf1 . elements) ['A'..'z']
+
 spec :: Spec
 spec = with (app $ readIO "2013-04-11 21:37:58 UTC") $ do
   describe "GET /" $ do
@@ -27,3 +33,8 @@ spec = with (app $ readIO "2013-04-11 21:37:58 UTC") $ do
   context "when given an invalid request path" $ do
     it "responds with HTTP status 404" $ do
       get "/some-path" `shouldRespondWith` 404
+
+  context "when given an *arbitrary* invalid request path" $ do
+    it "responds with HTTP status 404" $ do
+      property $ \(InvalidPath path) -> do
+        get path `shouldRespondWith` 404
